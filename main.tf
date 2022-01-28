8619cc94f0a6dcfb713bc1dd0b4282098b241fd0
Josh Sandlin <joshua.sandlin@gmail.com>
2022-01-27 23:24:59 -0500

    fix name

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "google_compute_instance" "nginx" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.nginx.address
+      nat_ip = google_compute_global_address.nginx.address
     }
   }
 
